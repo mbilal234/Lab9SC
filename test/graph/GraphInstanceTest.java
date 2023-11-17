9f4c1db334cc307a9129f0e9dc180215da1c4a4f
Bilal <muhammad.bilal.3446574@gmail.com>
2023-11-17 11:13:59 +0500

    Added test cases to GraphInstanceTest
    I have added the required test cases to graphinstancetest.java for all instance methods.

--- test/graph/GraphInstanceTest.java
+++ test/graph/GraphInstanceTest.java
@@ -4,43 +4,112 @@
 package graph;
 
 import static org.junit.Assert.*;
-
 import java.util.Collections;
+import java.util.Map;
 
 import org.junit.Test;
 
 /**
  * Tests for instance methods of Graph.
- * 
- * <p>PS2 instructions: you MUST NOT add constructors, fields, or non-@Test
- * methods to this class, or change the spec of {@link #emptyInstance()}.
- * Your tests MUST only obtain Graph instances by calling emptyInstance().
- * Your tests MUST NOT refer to specific concrete implementations.
  */
 public abstract class GraphInstanceTest {
-    
-    // Testing strategy
-    //   TODO
-    
+
     /**
      * Overridden by implementation-specific test classes.
      * 
      * @return a new empty graph of the particular implementation being tested
      */
     public abstract Graph<String> emptyInstance();
-    
-    @Test(expected=AssertionError.class)
+
+    @Test(expected = AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
-    
+
     @Test
     public void testInitialVerticesEmpty() {
-        // TODO you may use, change, or remove this test
-        assertEquals("expected new graph to have no vertices",
+        // Verify that a new graph has no vertices.
+        assertEquals("Expected new graph to have no vertices",
                 Collections.emptySet(), emptyInstance().vertices());
     }
-    
-    // TODO other tests for instance methods of Graph
-    
+
+    @Test
+    public void testAddVertex() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.add("A"));
+        assertTrue(graph.vertices().contains("A"));
+    }
+
+    @Test
+    public void testAddDuplicateVertex() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.add("A"));
+        assertFalse(graph.add("A"));
+    }
+
+    @Test
+    public void testAddEdge() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        assertEquals(0, graph.set("A", "B", 3));
+        assertTrue(graph.targets("A").containsKey("B"));
+        assertTrue(graph.sources("B").containsKey("A"));
+    }
+
+    @Test
+    public void testRemoveVertex() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        assertTrue(graph.remove("A"));
+        assertFalse(graph.vertices().contains("A"));
+    }
+
+    @Test
+    public void testRemoveNonexistentVertex() {
+        Graph<String> graph = emptyInstance();
+        assertFalse(graph.remove("A"));
+    }
+
+    @Test
+    public void testSetEdgeWeight() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        assertEquals(0, graph.set("A", "B", 3));
+        assertEquals(3, graph.set("A", "B", 5));
+    }
+
+    @Test
+    public void testRemoveEdge() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        graph.set("A", "B", 3);
+        assertEquals(3, graph.set("A", "B", 0));
+        assertFalse(graph.targets("A").containsKey("B"));
+        assertFalse(graph.sources("B").containsKey("A"));
+    }
+
+    @Test
+    public void testGetSources() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        graph.set("A", "B", 3);
+        Map<String, Integer> sources = graph.sources("B");
+        assertTrue(sources.containsKey("A"));
+        assertEquals(3, sources.get("A").intValue());
+    }
+
+    @Test
+    public void testGetTargets() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        graph.set("A", "B", 3);
+        Map<String, Integer> targets = graph.targets("A");
+        assertTrue(targets.containsKey("B"));
+        assertEquals(3, targets.get("B").intValue());
+    }
 }
